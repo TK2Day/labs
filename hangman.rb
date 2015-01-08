69cd5379619fc64baba8d5175864cd787ad60e03
DM <DungeonMaster@DMs-MacBook-Air.local>
2015-01-08 09:45:14 -0500

    Hey I have my hw but i did use help

--- hangman.rb
+++ hangman.rb
@@ -1,49 +1,94 @@
-## BEWARE THE SEMICOLON!
+## BEWARE THE SEICOLON
+#words[rand(words.length)]
 
-require 'pry'
-require 'set'
 
 words = ['angus',
-         'onomatopeia',
-         'mississippi',
-         'cookies',
-         'terminal',
-         'illness',
-         'communist',
-         'dictator',
-         'capitalist',
-         'marxist',
-         'darwinism']
+  'northrend',
+  'warcraft',
+  'counterstrike',
+  'counterterrorist',
+  'artyom',
+  'youtube',
+  'leagueoflegends',
+  'marxist',
+  'darwinism',
+  'orgrimmar',
+  'atlanta',
+  'konamicode',
+  'empires'
+  ]
 
-def finished? (turns, guesses, answer)
-  turns.zero? || answer.chars.all? { |l| guesses.include?(l) }
+guesses = 6
+wrong_guesses = 0
+right_letter = ''
+word = words[rand(words.length) - 1]
+
+
+
+
+
+def to_placeholders testw, guessedw
+  placeholder = ''
+  testw.chars {|char| placeholder += (guessedw.include? char)? char : '_ '
+  }
+  placeholder
 end
 
-def greeting
-  # TODO: tell the player about hangman
-end
+puts `clear`
+puts 'Can you get this word right? :'+ to_placeholders(word, '')
 
-def game_over
-  # TODO: tell the player what the word was and if they won or lost
-end
+while true
+  print "Guess a letter! You have [#{guesses - wrong_guesses}] tries left "
 
-def prompt_player
-  # TODO: get user input / show some status
-end
+  char = gets.chomp
 
-def hangman(words)
-  turn_count = argv.empty? ? 6 : argv[0].to_i
-  guessed = set.new
-  answer = words.sample(1)[0]
-  greeting # TODO: Do I need an argument?
-  until finished?(turn_count, guessed, answer)
-    guess = prompt_player
-    guesses.add(guess)
-    unless answer.include?(guess)
-      turn_count -= 1
+  if word.include? char
+
+    if(right_letter.include? char)
+      puts char + 'This letter has already been entered!'
+      puts 'Please enter another letter ' + to_placeholders(word, right_letter)
+    else
+      right_letter = right_letter + char
+      placeholder = to_placeholders(word, right_letter)
+
+      puts 'Nice one! ' + placeholder
+    end
+    unless placeholder.include? '_ '
+      puts "Congrats! All our base belong to you!"
+      break
+    end
+
+  else
+    puts "Sorry, not even close!"
+    wrong_guesses += 1
+
+    if wrong_guesses == guesses
+      puts "Sorry game over! The word was "
+      puts word
+    else
+      puts 'Try again! ' + to_placeholders(word, right_letter)
     end
   end
-  game_over # TODO: Do I need an argument?
 end
 
-hangman(words)
+# def finished? (turns,guesses, answer)
+#  turns.zero? || answer.chars.all { |l| guesses.include?(l) }
+# end
+
+
+
+
+# def hangman(words)
+#   turn_count = ARGV.empty? ? 6 : ARGV(0).to_i
+#   guessed = set.new
+#   answer = words.sample(1)[0]
+#   puts greeting
+#   until finished?(turn_count, guessed, answer)
+#     guess = prompt_player
+#     guesses.add(guess)
+#     unless answer.include?(guess)
+#     turn_count -= 1
+#   end
+#   end
+#   game_over # TODO: Do Ineed an argument?
+# end
